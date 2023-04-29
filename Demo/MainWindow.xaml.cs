bc71578eecfac416f1a527155dd4b85956d276c5
arseniy <samoylov6566@gmail.com>
2023-04-29 17:23:48 +0300

    Initial

---
+++ Demo/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Demo
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var listAllUsers = new List<User>();
+            var enter = false;
+            using (DBDemoEntities db = new DBDemoEntities())
+            {
+                foreach(User i in db.User)
+                {
+                    if(i.UserLogin == TextBoxEnter.Text && i.UserPassword == PasswordBoxEnter.Password)
+                    {
+                        enter = true;
+                        break;
+                    }
+                }
+            }
+            if (enter)
+            {
+                ProductWindow productWindow = new ProductWindow();
+                productWindow.Show();
+            }
+            else
+            {
+                MessageBox.Show("Проверьте правильность данных, пользовательне найден.");
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            
+            ProductWindow productWindow = new ProductWindow();
+            productWindow.Show();
+        }
+    }
+}
---
+++ Demo/ProductWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.Entity;
+
+namespace Demo
+{
+    /// <summary>
+    /// Логика взаимодействия для ProductWindow.xaml
+    /// </summary>
+    public partial class ProductWindow : Window
+    {
+        public ProductWindow()
+        {
+            
+            InitializeComponent();
+            using (DBDemoEntities db = new DBDemoEntities())
+            {
+                ProductList.ItemsSource = db.Product.ToList<Product>();
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            using (DBDemoEntities db = new DBDemoEntities())
+            {
+                ProductList.ItemsSource = db.Product.ToList<Product>().OrderByDescending(x => x.ProductCost);
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            using (DBDemoEntities db = new DBDemoEntities())
+            {
+                ProductList.ItemsSource = db.Product.ToList<Product>().OrderBy(x => x.ProductCost);
+                
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            using (DBDemoEntities db = new DBDemoEntities())
+            {
+                ProductList.ItemsSource = db.Product.ToList<Product>();
+            }
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            using (DBDemoEntities db = new DBDemoEntities())
+            {
+                ProductList.ItemsSource = db.Product.ToList<Product>().Where(x => x.ProductMaxDiscountAmount<10);
+
+            }
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            using (DBDemoEntities db = new DBDemoEntities())
+            {
+                ProductList.ItemsSource = db.Product.ToList<Product>().Where(x => x.ProductMaxDiscountAmount >= 10 && x.ProductMaxDiscountAmount<15);
+
+            }
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            using (DBDemoEntities db = new DBDemoEntities())
+            {
+                ProductList.ItemsSource = db.Product.ToList<Product>().Where(x => x.ProductMaxDiscountAmount >= 15);
+
+            }
+        }
+    }
+}
